68e26672e02fdf02eb85fc99d8595549a49107e6
Mateus Moreira <mateuuusss@gmail.com>
2023-11-08 20:58:36 -0300

    Meu primeiro dojo em git/gitflow
    Co-Authored-by: Bruno Bragança <brunobragancadosreis@gmail.com>
    Co-Authored-by: Eduardo Belarmino <eduardo83204@gmail.com>

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    c = a + b
+
+    print("The sum of", a, "and", b, "is", c)
+
+if __name__ == "__main__":
+    main()
